4289b94bb1d489890b072a6a4ce048588ea0001b
Iqbal Pahlevi Amin <79355631+iqbalpa@users.noreply.github.com>
2021-09-25 14:31:10 +0700

    upload hasil lab 0

---
+++ Lab/Lab00/biodata_anak_pacil.py
@@ -0,0 +1,21 @@
+# Mencetak judul porgram ini
+print("Biodata Anak Pacil 2021")
+
+# Mengambil input dari pengguna dan
+# menyimpannya ke dalam variabel-variabel
+nama = input("Nama: ")
+alamat = input("Alamat: ")
+tempat_lahir = input("Tempat Lahir: ")
+tanggal_lahir = input("Tanggal Lahir: ")
+hobi = input("Hobi: ")
+makanan_kesukaan = input("Makanan Kesukaan: ")
+
+# Mencetak isi dari variabel-variabel
+print()
+print("Biodataku: ")
+print(nama)
+print(alamat)
+print(tempat_lahir)
+print(tanggal_lahir)
+print(hobi)
+print(makanan_kesukaan)
